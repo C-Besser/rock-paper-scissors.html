4203558c6d88e26abcdc34c6cd9cfce8c54474cd
C-Besser <106854840+C-Besser@users.noreply.github.com>
2023-04-12 20:43:28 -0500

    Edit README, create html and js files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rock-paper-scissors.html
+create original html and readme 4/12/2023
---
+++ main.js
@@ -0,0 +1,14 @@
+create function for getComputerChoice
+assign getComputerChoice to computerSelection
+
+receive input from user of Rock Paper or Scissors
+assign user input to playerSelection
+make selection case-insensitive
+
+compare playerSelection to computerSelection
+
+return string declaring winner
+
+function game() to reset game 5 Time
+
+keep count of games won and lost
