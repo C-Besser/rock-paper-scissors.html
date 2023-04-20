ab4c0ea4c8dae1a2e23646a06375d5380b764b08
C-Besser <106854840+C-Besser@users.noreply.github.com>
2023-04-19 20:55:45 -0500

    Add playRound function. Add alert for user typos

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const gameChoices = ['rock', 'paper', 'scissors'];
+//create function for getComputerChoice
+//assign getComputerChoice to computerSelection
 
 function getComputerChoice(array){
     const random = Math.floor(Math.random()*array.length);
@@ -9,18 +11,44 @@ let computerSelection = getComputerChoice(gameChoices);
 
 console.log(computerSelection);
 
-  
-//create function for getComputerChoice
-//assign getComputerChoice to computerSelection
-
-let playerSelection = prompt("Let's Play! Choose One: ROCK, PAPER, SCISSORS");
-console.log(playerSelection);
-
 //receive input from user of Rock Paper or Scissors
 //assign user input to playerSelection
 //make selection case-insensitive
+  
+
+let playerSelection = prompt("Let's Play! CHOOSE ONE: rock, paper, or scissors");
+console.log(playerSelection);
+
+playerSelection = playerSelection.toLowerCase();
+
+console.log(playerSelection);
+
 
 //compare playerSelection to computerSelection
+
+function playRound(playerSelection, computerSelection) {
+  if playerSelection === computerSelection {
+    return `${playerSelection} ties with ${computerSelection}!`;
+  } else if playerSelection = 'rock' {
+    if computerSelection = 'scissors' {
+      return "You lose! Scissors beats rock!";
+    }
+    else computerSelection = 'paper'{
+      return "You win! Rock beats paper!";
+    }
+} else if playerSelection = 'paper' {
+
+}
+
+if playerSelection === 'rock' {
+  playRound();
+} else if playerSelection === 'paper' {
+  playRound();
+} else if playerSelection === 'scissors' {
+  playRound();
+} else {
+  alert("You did not enter a valid choice.")
+}
 
 //return string declaring winner
 
